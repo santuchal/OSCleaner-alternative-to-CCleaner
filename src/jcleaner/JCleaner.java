ed3e0f8ccc65c20d42f8bbe8731df6f60957aa26
santu <santu.app@gmail.com>
2015-12-11 12:49:32 +0530

    First Commit

---
+++ src/jcleaner/JCleaner.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jcleaner;
+
+/**
+ *
+ * @author pc1
+ */
+public class JCleaner {
+
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        home h1 = new home();
+        h1.setVisible(true);
+        
+
+
+    // TODO code application logic here
+    }
+    
+}
